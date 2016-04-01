a2e7d8159fec96abd6ef59c76b20f9ffe19165d7
Anna Durham <durham.anna1@gmail.com>
2016-03-31 20:53:03 -0400

    drag and drop works

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react-addons-update';
 
 class LaneStore {
 	constructor() {
@@ -39,6 +40,10 @@ class LaneStore {
 
 	attachToLane({laneId, noteId}) {
 		const lanes = this.lanes.map(lane => {
+			if(lane.notes.includes(noteId)) {
+				lane.notes = lane.notes.filter(note => note !== noteId);
+			}
+
 			if(lane.id === laneId) {
 				if(lane.notes.includes(noteId)) {
 					console.warn('Already attached note to lane', lanes);
@@ -63,6 +68,32 @@ class LaneStore {
 
 		this.setState({lanes});
 	}
+
+
+move({sourceId, targetId}) {
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter(lane => lane.notes.includes(sourceId))[0];
+    const targetLane = lanes.filter(lane => lane.notes.includes(targetId))[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if(sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+
+    this.setState({lanes});
+  }
+
 }
 
 export default alt.createStore(LaneStore, 'LaneStore');
